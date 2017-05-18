4c41f2c60132b204350187edb490ae98c8d164ec
Graham Crocker <gpcrocker@gmail.com>
2017-05-19 00:11:45 +0100

    Move autoloading to bootstrap file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,8 @@
 
 const DOT_ENV = __DIR__ . '/../';
 
+require __DIR__ . '/../vendor/autoload.php';
+
 if (file_exists(DOT_ENV . '/.env')) {
     $dotEnv = new \Dotenv\Dotenv(DOT_ENV);
     $dotEnv->load();
--- public/index.php
+++ public/index.php
@@ -6,8 +6,6 @@ if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
 
-require __DIR__ . '/../vendor/autoload.php';
-
 session_start();
 
 require __DIR__ . '/../app/bootstrap.php';
